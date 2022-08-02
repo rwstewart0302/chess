9da244f83e90638298759c0ef2e354dbbaca7128
Stewart, Robert <rstewart@lmi.org>
2022-08-02 18:16:21 -0400

    updated chess movement

--- chess.py
+++ chess.py
@@ -8,34 +8,72 @@ SQUARESIZE = 100 # pixels per square
 RANKS = 8
 FILES = 8
 
+W_PAWN = 'WP'
+W_ROOK = 'WR'
+W_KNIGHT = 'WN'
+W_BISHOP = 'WB'
+W_QUEEN = 'WQ'
+W_KING = 'WK'
+
+B_PAWN = 'BP'
+B_ROOK = 'BR'
+B_KNIGHT = 'BN'
+B_BISHOP = 'BB'
+B_QUEEN = 'BQ'
+B_KING = 'BK'
+
 
 def create_board():
-    board = np.zeros((RANKS, FILES))
+    board = np.full((RANKS, FILES), '00')
     for n_row in range(RANKS):
         for ele in range(FILES):
-            if n_row == 0 or n_row == 7:
+            if n_row == 1:
+                board[n_row, ele] = W_PAWN
+            elif n_row == 0:
                 if ele == 0 or ele == 7:
-                    board[n_row, ele] = 5
+                    board[n_row, ele] = W_ROOK
                 elif ele == 1 or ele == 6:
-                    board[n_row, ele] = 2
+                    board[n_row, ele] = W_KNIGHT
                 elif ele == 2 or ele == 5:
-                    board[n_row, ele] = 3
+                    board[n_row, ele] = W_BISHOP
                 elif ele == 3:
-                    board[n_row, ele] = 9
+                    board[n_row, ele] = W_QUEEN
                 elif ele == 4:
-                    board[n_row, ele] = 1_000
-            elif n_row == 1 or n_row == 6:
-                board[n_row, ele] = 1
-    print(board)
+                    board[n_row, ele] = W_KING
+
+            if n_row == 6:
+                board[n_row, ele] = B_PAWN
+            elif n_row == 7:
+                if ele == 0 or ele == 7:
+                    board[n_row, ele] = B_ROOK
+                elif ele == 1 or ele == 6:
+                    board[n_row, ele] = B_KNIGHT
+                elif ele == 2 or ele == 5:
+                    board[n_row, ele] = B_BISHOP
+                elif ele == 3:
+                    board[n_row, ele] = B_QUEEN
+                elif ele == 4:
+                    board[n_row, ele] = B_KING
+
+    return np.flip(board, 0)
 
 
 def main():
     player = 'white'
+    board = create_board()
     while True:
-        board = create_board()
         print(board)
         piece_loc = eval(input('Select a row and column: '))
-        if board[piece_loc[0], piece_loc[1]] ==
+        r_start = piece_loc[0]
+        c_start = piece_loc[1]
+        print('starting piece: ', board[r_start, c_start])
+        if board[r_start, c_start] == W_PAWN:
+            pawn_move = cp.PieceMovement(board)
+            piece_move_to = eval(input('Select a row and column: '))
+            r_end = piece_move_to[0]
+            c_end = piece_move_to[1]
+            print('square to move to: ', board[r_end, c_end])
+            board = pawn_move.move(piece=W_PAWN, player='white', r_start=r_start, c_start=c_start, r_end=r_end, c_end=c_end, board=board)
 
 if __name__ == '__main__':
     main()
--- chess_pieces.py
+++ chess_pieces.py
@@ -1,13 +1,43 @@
-class Pawn:
-    def __init__(self):
-        self.pawn = 1
+W_PAWN = 'WP'
+W_ROOK = 'WR'
+W_KNIGHT = 'WN'
+W_BISHOP = 'WB'
+W_QUEEN = 'WQ'
+W_KING = 'WK'
 
-    def move(player):
+B_PAWN = 'BP'
+B_ROOK = 'BR'
+B_KNIGHT = 'BN'
+B_BISHOP = 'BB'
+B_QUEEN = 'BQ'
+B_KING = 'BK'
+
+class PieceMovement:
+    def __init__(self, board):
+        self.board = board
+        self.piece = None
+        self.player = None
+        self.r_start = None
+        self.c_start = None
+        self.r_end = None
+        self.c_end = None
+
+
+    def move(self, piece, player, r_start, c_start, r_end, c_end, board):
+        self.player = player
+        self.piece = piece
+        self.r_start = r_start
+        self.c_start = c_start
+        self.r_end = r_end
+        self.c_end = c_end
         if player == 'white':
-            try:
-                
+            if piece == W_PAWN:
+                if board[r_end, c_end] = '00':
+                    board[r_end, c_end] = piece
+
         elif player == 'black':
             pass # move black pawn pieces
+        return board
 
 class Rook:
     def __init__(self):
